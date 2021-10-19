2623ac4ebe07ac6f8dbbde3d2fa133de1e31687e
Thara Supasiti <sapiensis@gmail.com>
2021-10-19 12:51:43 +1100

    setup project

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+
+// const { typeDefs, resolvers, context } = require('./schemas');
+const db = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context,
+// });
+// server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+// app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    // console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
